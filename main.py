6a85c228b0633efdaa97dbe3d54e3bfa13f3cdda
narms <75282394+n-arms@users.noreply.github.com>
2021-03-01 08:45:30 -0500

    Use subprocess.run to open generated files in default text editor

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import os, sys, json
+import os, sys, json, subprocess
+
+def usage():
+    print("usage: problem <problemname> <language>")
+
+def open_file(name, lang, config):
+    subprocess.run(["open", "-a", config["default-app"], name+"/"+config["languages"][lang][0]["filename"]])
 
 def load_json():
     with open("config.json") as config_json_file:
         return json.load(config_json_file)
-
 
 def parse_args(config):
     for i in sys.argv:
@@ -16,9 +21,15 @@ def parse_args(config):
 def main():
     config = load_json()
     lang, name = parse_args(config)
+    if (name is None or lang is None):
+        usage()
+        return
+
     os.mkdir(name)
     for file_pattern in config["languages"][lang]:
         with open(name+"/"+file_pattern["filename"], "w") as current_file:
             current_file.write(file_pattern["contents"])
+
+    open_file(name, lang, config)
 
 main()

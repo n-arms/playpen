283c739179638976f1524c70b9536ec55581e96b
narms <75282394+n-arms@users.noreply.github.com>
2021-02-28 21:09:58 -0500

    Implement basic main.py features
    * oh wow bash is so bad compared to python

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import os, sys, json
 
-with open("config.json") as config_json_file:
-    config = json.load(config_json_file)
+def load_json():
+    with open("config.json") as config_json_file:
+        return json.load(config_json_file)
+
+
+def parse_args(config):
+    for i in sys.argv:
+        if i in config["languages"]:
+            lang = i
+        else:
+            name = i
+    return (lang, name)
+
+def main():
+    config = load_json()
+    lang, name = parse_args(config)
+    os.mkdir(name)
+    for file_pattern in config["languages"][lang]:
+        with open(name+"/"+file_pattern["filename"], "w") as current_file:
+            current_file.write(file_pattern["contents"])
+
+main()
